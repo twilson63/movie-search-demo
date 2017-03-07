af7241dd3e082d66a7ea8678ac5ff2a931588485
Tom Wilson <tom@jackhq.com>
2017-03-07 11:45:51 -0500

    feat(app): Movie Search App
    Created Movie Search App using React Components
    
    first commit

---
+++ src/App.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react'
+import 'tachyons/css/tachyons.css'
+import fetch from 'isomorphic-fetch'
+import Search from './components/search'
+import Posters from './components/posters'
+
+import { propOr } from 'ramda'
+
+
+const url = 'http://www.omdbapi.com/'
+
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      movies: [],
+      error: '',
+      searchedFor: ''
+    }
+    this.search = this.search.bind(this)
+  }
+
+  search ({searchText, searchType}) {
+    fetch(`${url}?s=${searchText}&type=${searchType.toLowerCase()}`)
+      .then(res => res.json())
+      .then(res => {
+        return res
+      })
+      .then(json => this.setState({
+        searchedFor: searchText,
+        movies: propOr([], 'Search', json),
+        error: json.Error
+      }))
+      .catch(err =>
+        this.setState({
+          error: 'Error Occured: Try Again',
+          movies: [],
+          searchedFor: ''
+      }))
+  }
+
+  render() {
+    return (
+      <div className="pa4">
+        <h1>Movie Search</h1>
+        <Search search={this.search} />
+        {this.state.error ? <h2>{this.state.error}</h2> : null}
+        {this.state.searchedFor ? <p>Searched for: {this.state.searchedFor}</p> : null}
+        <Posters movies={this.state.movies} />
+      </div>
+    );
+  }
+}
+
+export default App;
